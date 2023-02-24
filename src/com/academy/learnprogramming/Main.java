a4e1db3f03156cf722b8dad93e4801568ea7be8c
Denis Hirschhorn <Denishirschh@hotmail.com>
2023-02-23 23:01:18 -0500

    right path example2

--- src/com/academy/learnprogramming/Main.java
+++ src/com/academy/learnprogramming/Main.java
@@ -1,8 +1,22 @@
 package com.academy.learnprogramming;
 
+import com.sun.source.tree.Tree;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Solution solution = new Solution();
+//    solution.maxDepth(new Solution.TreeNode(1,null,new Solution.TreeNode(2,new Solution.TreeNode(3,null,new Solution.TreeNode
+//            (4,null,new Solution.TreeNode(5,new Solution.TreeNode(6,new Solution.TreeNode(7,null,null),null),null))),null)));
+
+      solution.maxDepth(new Solution.TreeNode(3,new Solution.TreeNode(9),new Solution.TreeNode(20,new Solution.TreeNode(15),new Solution.TreeNode(7))));
+//      solution.maxDepth(new Solution.TreeNode(0));
+//        solution.maxDepth(new Solution.TreeNode(1,new Solution.TreeNode(2, new Solution.TreeNode(4), new Solution.TreeNode(5)), new Solution.TreeNode(3)));
+//        solution.maxDepth(new Solution.TreeNode(1,null,new Solution.TreeNode(2)));
+//          solution.maxDepth(new Solution.TreeNode(1,new Solution.TreeNode(2),null));
+//    solution.maxDepth(new Solution.TreeNode(-8,new Solution.TreeNode(-6,new Solution.TreeNode(6,null,new Solution.TreeNode(5)),null),new Solution.TreeNode(-7)));
+
+
+
     }
 }
--- src/com/academy/learnprogramming/Solution.java
+++ src/com/academy/learnprogramming/Solution.java
@@ -5,10 +5,11 @@ import com.sun.source.tree.Tree;
 import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Solution {
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -31,54 +32,163 @@ public class Solution {
     int rightDepth = 0;
     int tempDepth = 0;
     int tempDepth2 = 0;
-
+    int totalDepth = 1;
     TreeNode leftBranch = null;
     TreeNode rightBranch = null;
     TreeNode rightLeaf = null;
     TreeNode leftLeaf = null;
-    ArrayList<TreeNode> leafPile = new ArrayList<>();
-    ArrayList<TreeNode> leafPile2 = new ArrayList<>();
+    LinkedList<TreeNode> leafPile = new LinkedList<>();
+    boolean button;
+
 
     public int maxDepth(TreeNode root) {
-
-        if (rightBranch.right != null) {
-            rightBranch = rightBranch.right;
-            rightDepth++;
-            rightStuff(rightBranch);
-            //method call
-            if (leafPile.size() > 0) {
-                tempDepth = rightDepth;
-                rightDepth = 3;
-                for (int i = 0; i < leafPile.size(); i++) {
-                    if (rightDepth > tempDepth) {
-                        tempDepth = rightDepth;
-                        rightDepth = 3 + i;
-                    }
-                    if (leafPile.get(i).right != null) {
-                        rightStuff(leafPile.get(i));
-                    }
-                    if (leafPile.get(i).left != null) {
-                        leafPile2.add(leafPile.get(i).left);
-
-                    }
+        if (root == null){
+            System.out.println("returning 0");
+            return 0;
+        }
+        if (root.right == null && root.left == null){
+            System.out.println("returning 1");
+            return 1;
+        }
+        if (root.right == null){
+            System.out.println("root.right == null");
+            if (root.left != null) {
+//                leftDepth++;
+                leftStuff(root.left);
+                System.out.println("raking pile due to null root.right");
+                while (!leafPile.isEmpty()){
+                    leafRaker();
                 }
             }
         }
-
-
-        return root.val;
+        if (root.right != null) {
+            rightStuff(root.right);
+            while (!leafPile.isEmpty()) {
+                System.out.println("raking leaves due to non null root.right");
+                leafRaker();
+            }
+        }
+        if (root.left != null) {
+//            leftDepth++;
+            leftStuff(root.left);
+            while (!leafPile.isEmpty()){
+                System.out.println("raking leaves due to non null root.left");
+                leafRaker();
+            }
+        }
+        System.out.println("right is "+rightDepth+" left is "+leftDepth);
+        totalDepth = Math.max(rightDepth, leftDepth);
+        System.out.println("total depth is "+(totalDepth));
+        return (totalDepth);
     }
 
 
     public void rightStuff(TreeNode branch) {
-        while (rightBranch.right != null) {
-            if (rightBranch.left != null) {
-                leftLeaf = rightBranch.left;
-                leafPile.add(leftLeaf);
-            }
-            rightBranch = rightBranch.right;
+        if (rightDepth < 1){
+            rightDepth = 1;
+        }
+        rightDepth++;
+        System.out.println("right depth is now v1 "+rightDepth);
+        System.out.println("right branch.val is "+branch.val);
+        if (branch.right == null && branch.left == null){
             rightDepth++;
+            System.out.println("right depth is now v3 "+rightDepth);
+        }
+        if (branch.left != null) {
+            System.out.println("right outer loop add");
+            button = true;
+            if (!(leafPile.contains(branch.left))) {
+                System.out.println("adding " + branch.left.val + " to leaf pile");
+                leafPile.add(branch.left);
+            }
+        }
+        while (branch.right != null) {
+            rightDepth++;
+            System.out.println("right depth is now v2 "+rightDepth);
+            if (branch.left != null && !(leafPile.contains(branch.left))) {
+                System.out.println("right inner loop add");
+                button = true;
+                leafPile.add(branch.left);
+                System.out.println("adding " + branch.left.val + " to leaf pile");
+            }
+
+            if (branch.right != null) {
+                System.out.println("going right towards node " + branch.right.val);
+                branch = branch.right;
+                rightBranch = branch;
+            }
+            if (branch.right == null && branch.left != null){
+                button = true;
+                System.out.println("right inner add 3");
+                leafPile.add(branch.left);
+                System.out.println("adding " + branch.left.val + " to leaf pile");
+
+            }
         }
 
+
+        System.out.println("ending right stuff");
+    }
+
+    public void leafRaker() {
+        if (button) {
+            if (!leafPile.isEmpty()) {
+                System.out.println("raking right leaf pile! with " + leafPile.get(0).val);
+                rightStuff(leafPile.get(0));
+                leafPile.remove(leafPile.get(0));
+            }
+        }
+        if (!button) {
+            if (!leafPile.isEmpty()) {
+                System.out.println("raking left leaf pile! with " + leafPile.get(0).val);
+                leftStuff(leafPile.get(0));
+                leafPile.remove(leafPile.get(0));
+            }
+        }
+    }
+
+
+    public void leftStuff(TreeNode branch) {
+        System.out.println("branch.val is "+branch.val);
+        leftDepth ++;
+        if (branch.right == null && branch.left == null){
+            leftDepth++;
+            System.out.println("right depth is now v3 "+rightDepth);
+        }
+        System.out.println("left depth is "+leftDepth);
+        if (branch.right != null && !(leafPile.contains(branch.right))) {
+            System.out.println("left outer loop add");
+            button = false;
+            System.out.println("adding " + branch.right.val + " to leaf pile");
+            leafPile.add(branch.right);
+        }
+        while (branch.left != null) {
+            leftDepth++;
+            if (branch.right != null && !(leafPile.contains(branch.right))) {
+                leftDepth++;
+                System.out.println("left depth is "+leftDepth);
+                button = false;
+                System.out.println("left inner add 1");
+                leafPile.add(branch.right);
+                System.out.println("adding " + branch.right.val + " to leaf pile");
+            }
+            if (branch.left == null && branch.right != null && !(leafPile.contains(branch.right))) {
+                button = false;
+                System.out.println("left depth is "+leftDepth);
+                leftDepth++;
+                System.out.println("Left inner add 2");
+                leafPile.add(branch.right);
+                System.out.println("adding " + branch.right.val + " to leaf pile");
+            }
+            System.out.println("going left! with node " + branch.left.val);
+            branch = branch.left;
+            if (branch.left == null && branch.right != null){
+                button = false;
+                System.out.println("Left inner add 3");
+                leafPile.add(branch.right);
+                System.out.println("adding " + branch.right.val + " to leaf pile");
+            }
+        }
+        System.out.println("leftDepth " + leftDepth + " total depth " + totalDepth);
     }
 }
